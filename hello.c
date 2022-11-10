6aa06ddf897928c3c4937629535500280b235be9
Alex Axthelm <alex@asset-resolution.com>
2022-11-10 16:01:15 +0100

    Update hello message

--- hello.c
+++ hello.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 
 #ifndef DOCKER_IMAGE
-	#define DOCKER_IMAGE "hello-world"
+	#define DOCKER_IMAGE "docker-hello-PACTA"
 #endif
 
 #ifndef DOCKER_GREETING
-	#define DOCKER_GREETING "Hello from Docker!"
+	#define DOCKER_GREETING "Hello from PACTA!"
 #endif
 
 #ifndef DOCKER_ARCH
@@ -20,21 +20,14 @@ const char message[] =
 	"\n"
 	"To generate this message, Docker took the following steps:\n"
 	" 1. The Docker client contacted the Docker daemon.\n"
-	" 2. The Docker daemon pulled the \"" DOCKER_IMAGE "\" image from the Docker Hub.\n"
+	" 2. The Docker daemon pulled the \"" DOCKER_IMAGE "\" image from the PACTA container registry.\n"
 	"    (" DOCKER_ARCH ")\n"
 	" 3. The Docker daemon created a new container from that image which runs the\n"
 	"    executable that produces the output you are currently reading.\n"
 	" 4. The Docker daemon streamed that output to the Docker client, which sent it\n"
 	"    to your terminal.\n"
-	"\n"
-	"To try something more ambitious, you can run an Ubuntu container with:\n"
-	" $ docker run -it ubuntu bash\n"
-	"\n"
-	"Share images, automate workflows, and more with a free Docker ID:\n"
-	" https://hub.docker.com/\n"
-	"\n"
-	"For more examples and ideas, visit:\n"
-	" https://docs.docker.com/get-started/\n"
+  "\n"
+  "Thanks for confirming that you can pull from the PACTA container registry."
 	"\n";
 
 int main() {
